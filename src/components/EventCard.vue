a147ed569e58a8f6e5a9741d9dc2af487c936167
Zion Perez <zionperez@gmail.com>
2019-02-17 22:18:43 +0900

    Complete lesson 9: API Calls with Axios.

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,8 +1,5 @@
 <template>
-  <router-link
-    class="event-link"
-    v-bind:to="{ name: 'event-show', params: { id: '1' } }"
-  >
+  <router-link class="event-link" v-bind:to="{ name: 'event-show', params: { id: event.id } }">
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
@@ -13,19 +10,8 @@
 
 <script>
 export default {
-  data() {
-    return {
-      event: {
-        id: 1,
-        title: 'Park Cleanup',
-        date: 'Tues Aug 19 2019',
-        time: '6:00',
-        attendees: [
-          { id: 'abc123', name: 'Adam Jar' },
-          { id: 'qwe234', name: 'Gregg Pollack' }
-        ]
-      }
-    }
+  props: {
+    event: Object
   }
 }
 </script>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,17 +1,32 @@
 <template>
   <div>
     <h1>Event List</h1>
-    <EventCard />
+    <EventCard v-for="event in events" v-bind:key="event.id" :event="event"/>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
 
 export default {
   name: 'EventList',
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log('There was an error: ' + error.response)
+      })
   }
 }
 </script>
