a0a69737e1ad98f873cbc180531c1502fb3efb92
Zion Perez <zionperez@gmail.com>
2019-03-27 20:26:01 +0900

    Complete mastering vuex lesson 4: mutations and actions part1.

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
@@ -22,8 +23,18 @@ export default new Vuex.Store({
       { id: 4, text: '...', organizer: '...' }
     ]
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },  
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })      
+    }
+  },
   getters: {
     catLength: state => {
       return state.categories.length
